cb1a64c2d6e01cc4bcb47779ede5a4f18fda1475
Ployola <ployola@hiper.com.pe>
2025-07-24 14:30:21 -0500

    feat: new project

---
+++ channel-zer-auth/src/main/java/com/zer/lot/channel/zer/auth/ChannelZerAuthApplication.java
@@ -0,0 +1,13 @@
+package com.zer.lot.channel.zer.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChannelZerAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChannelZerAuthApplication.class, args);
+	}
+
+}
---
+++ channel-zer-auth/src/test/java/com/zer/lot/channel/zer/auth/ChannelZerAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zer.lot.channel.zer.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChannelZerAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
